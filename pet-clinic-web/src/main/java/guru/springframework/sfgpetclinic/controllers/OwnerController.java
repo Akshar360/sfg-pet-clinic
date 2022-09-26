3ed406481b2a580aa45c605cb6a69f812b764e79
Akshar <akshar.sivnarain@gmail.com>
2022-09-26 09:53:30 +0200

    Fixed find owners bug. Closes 61

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -63,7 +63,7 @@ public class OwnerController
         }
 
         // find owners by last name
-        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%");
+        List<Owner> results = ownerService.findAllByLastNameLike("%"+ owner.getLastName() + "%"); //fix for bug 61
 
         if (results.isEmpty())
         {
