e82c3d4e542ed49c80fec4ae18b8cab4981ddf28
Akshar <akshar.sivnarain@gmail.com>
2022-09-26 09:48:55 +0200

    Implemented find owner. Closes #54

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -28,10 +29,17 @@ public class OwnerSDJpaService implements OwnerService
     }
 
     @Override
-    public Set<Owner> findAll() {
+    public Set<Owner> findAll()
+    {
         Set<Owner> owners = new HashSet<>();
         ownerRepository.findAll().forEach(owners::add);
         return owners;
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName)
+    {
+        return ownerRepository.findAllByLastNameLike(lastName);
     }
 
     @Override
