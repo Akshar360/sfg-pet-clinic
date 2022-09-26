dd7d06ae71dd5e5634bb71721024a80896853d58
Akshar <akshar.sivnarain@gmail.com>
2022-09-26 05:02:21 +0200

    Fixed display owner. Closes #65.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -31,5 +31,5 @@ public class Pet extends BaseEntity
     private LocalDate birthDate;
 
     @OneToMany(cascade = CascadeType.ALL , mappedBy = "pet")
-    private Set<Visit> visitSet = new HashSet<>();
+    private Set<Visit> visits = new HashSet<>();
 }
